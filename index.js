0ab8594e27fb8b872f5d08c8846412ff0d300487
Test User <test@example.com>
2026-02-12 12:42:02 +0530

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 // Home route
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js app setup by rajkumar!');
+    res.send('Hello from Node.js app anjali');
 });
 
 // API route
